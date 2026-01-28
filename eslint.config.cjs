1baae80018e17b7fcb7a6637267dcfa200b72dd9
Enzo Picarel <enzo.picarel@bordeaux-inp.fr>
2026-01-28 17:12:06 +0100

    chore: add ESLint config

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    ignores: ["node_modules/**", ".nuxt/**"]
+  },
+  {
+    files: ["**/*.{ts,vue,js}"],
+    languageOptions: {
+      parser: require("vue-eslint-parser"),
+      parserOptions: {
+        parser: require("@typescript-eslint/parser"),
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+    },
+    // configuration minimale en format "flat" (ESLint v9)
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
+  }
+]
