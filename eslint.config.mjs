b8d21218a23024e7ee173305dea32f7f7fcbf86f
Enzo Picarel <enzo.picarel@bordeaux-inp.fr>
2026-02-11 17:37:31 +0100

    refactor: update ESLint configuration to include TypeScript files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import tsParser from '@typescript-eslint/parser'
 
-export default withNuxt({
-  files: ['**/*.vue'],
-  languageOptions: {
-    parserOptions: {
-      parser: '@typescript-eslint/parser'
+export default withNuxt(
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser
+      }
+    }
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser
     }
   }
-})
+)
